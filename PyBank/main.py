917946fb986116a497f75dfc9af257b0a17f0537
Brian Morton <bmorton925@gmail.com>
2021-01-13 12:07:28 -0500

    initial commit

---
+++ PyBank/main.py
@@ -0,0 +1,37 @@
+#module imports
+import os
+import csv
+
+#compress path to csv file
+path = os.path.join('Resources', 'budgetdata.csv').replace("\\","/")
+
+#open csv file as readonly and name
+budget_data = open(path,'r')
+
+#reader call and seperate file by comma
+reader = csv.reader(budget_data, delimiter=',')
+
+#for loop thru file
+for row in reader:
+   # print statement of file to see if working
+  print(row)
+    
+
+
+
+
+
+
+#sample code
+# # Method 1: Plain Reading of CSV files
+# with open(csvpath, 'r') as file_handler:
+#     lines = file_handler.read()
+#     print(lines)
+#     print(type(lines))
+
+#workaround solution for windows
+#if using \\ use command
+# .replace("\\","/") at end of line
+
+
+#'..', 'Pybank',
